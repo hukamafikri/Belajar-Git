a625ec1a394bed91eb3e780f8180e09b81ee806a
hukamafikri <69191400+hukamafikri@users.noreply.github.com>
2020-08-11 21:30:28 +0700

    Add files via upload

---
+++ Boolean.kt
@@ -0,0 +1,15 @@
+fun main(){
+    val nilai = 49
+
+    if (nilai > 90){
+        println("A")
+    } else if (nilai > 80){
+        println("B")
+    } else if (nilai > 70){
+        println("C")
+    } else if (nilai > 60){
+        println("D")
+    } else {
+        println("E")
+    }
+}
