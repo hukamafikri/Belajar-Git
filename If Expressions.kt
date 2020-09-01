191ef5bf8c9deaed056383dfd6b199072b14dc8e
hukamafikri <69191400+hukamafikri@users.noreply.github.com>
2020-09-01 19:55:41 +0700

    Delete If Expressions.kt

--- If Expressions.kt
+++
@@ -1,9 +0,0 @@
-fun main(){
-    val nilai = 90
-
-    if (nilai >= 75){
-        println("Nilai kamu $nilai, selamat ya!")
-    } else {
-        println("Nilai kamu $nilai, silahkan ikuti remedial")
-    }
-}
