17c238b152bfb364c40cfe9b9c1bd975ba756d51
hukamafikri <69191400+hukamafikri@users.noreply.github.com>
2020-09-01 19:55:10 +0700

    Delete Functions.kt

--- Functions.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-    val user = setUser("Hukama Fikri", "Gaming", 17)
-    println(user)
-
-    printUser("Hukama Fikri")
-}
-    fun setUser(name: String, hobby: String, age: Int) = "My name is $name, I like $hobby, and i'm $age years old"
-    fun printUser(name: String) {
-}
